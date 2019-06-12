fffd8690764567b4285ce550a88c1bbd0c6b5c65
Heather Miller <heather.miller114@gmail.com>
2019-06-12 11:07:12 -0400

    adds validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
     has_many :cities, through: :wishlists
 
     has_secure_password
+
+    validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL }, uniqueness: { case_sensitive: false }
+
 end
