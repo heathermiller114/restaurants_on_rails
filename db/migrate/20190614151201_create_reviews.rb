ed17cebeff05b2f5fbb6c2846a9363a22e38c551
Heather Miller <heather.miller114@gmail.com>
2019-06-14 11:17:09 -0400

    fixes syntax error in reviews table

--- db/migrate/20190614151201_create_reviews.rb
+++ db/migrate/20190614151201_create_reviews.rb
@@ -4,8 +4,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :content
       t.integer :star_rating
-      t.belongs_to :user, foreign_key :true
-      t.belongs_to :restaurant, foreign_key :true
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :restaurant, foreign_key: true
 
       t.datetime :created_at, null: false
       t.datetime :updated_at, null: false
