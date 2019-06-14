babd673513fa4541be6237fbfba2696333ed85dd
Heather Miller <heather.miller114@gmail.com>
2019-06-14 11:54:44 -0400

    updates users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        session[:user_id] = @user.id
-
-        redirect_to user_path(@user)
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
 
     end
 
