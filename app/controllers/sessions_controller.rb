6ab878fdd6b29c9bc879e5b5687f870e24ae7b41
Heather Miller <heather.miller114@gmail.com>
2019-06-16 19:11:56 -0400

    adds sessions#create logic for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,15 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        #byebug
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry, we were unable to log you in"
+            redirect_to login_path
+        end
     end
 
     def destroy
