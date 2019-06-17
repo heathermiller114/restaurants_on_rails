903d1da4f279935fa11d8cfd0d2be21bd52a23a0
Heather Miller <heather.miller114@gmail.com>
2019-06-16 20:03:17 -0400

    adds google omniauth route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get '/auth/google/callback', to 'sessions#omniauth'
+
   resources :restaurants
   resources :cities
 end
