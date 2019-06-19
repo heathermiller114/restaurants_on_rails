83617e2039eb2930f03725d175774eca178debe4
Heather Miller <heather.miller114@gmail.com>
2019-06-19 15:34:44 -0400

    adds restaurants#create method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,12 @@ class RestaurantsController < ApplicationController
     end
 
     def create
-
+        @restaurant = Restaurant.new(restaurant_params)
+        if @restaurant.save
+            redirect_to restaurant_path(@restaurant)
+        else
+            render :new
+        end
     end
 
     private
