bbebb75f36b85bf141715a282c02ee37df37d5be
Heather Miller <heather.miller114@gmail.com>
2019-06-30 16:37:03 -0400

    adds new reviews form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,9 +3,11 @@ class ReviewsController < ApplicationController
     def index
 
     end
-    
-    def new
 
+    def new
+        #byebug
+        @restaurant = Restaurant.find_by(id: params[:restaurant_id])
+        @review = @restaurant.build_review
     end
 
     def create
