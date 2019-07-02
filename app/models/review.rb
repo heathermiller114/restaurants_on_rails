ab5f032d8c833c6decd2bd7baef303655d78bd51
Heather Miller <heather.miller114@gmail.com>
2019-07-02 14:58:20 -0400

    fixes syntax of numericality validations for star_rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
     belongs_to :restaurant
 
     validates :content, presence: true, length: { maximum: 500 }
-    validates :star_rating, numericality: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 
+    validates_numericality_of :star_rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5 
 end
