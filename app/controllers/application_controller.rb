1539e75b2ddc2af623d2973a212440e0ad18ec2d
Heather Miller <heather.miller114@gmail.com>
2019-07-02 16:05:33 -0400

    adds current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    private
+        def current_user
+            @current_user ||= User.find_by_id(session[:user_id])
+            #or/equals will only call DB once if using it 1+ times in a method
+        end
+
 end
