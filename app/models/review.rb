2e030ebe78d81e2e8d47d272b697db0a82c1fd5e
Heather Miller <heather.miller114@gmail.com>
2019-07-03 22:42:34 -0400

    adds high to low scope method and link on reviews index page

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,7 @@ class Review < ApplicationRecord
 
     validates :content, presence: true, length: { maximum: 500 }
     validates_numericality_of :star_rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5 
+
+    scope :high_to_low, -> { order(star_rating: :desc)}
+
 end
