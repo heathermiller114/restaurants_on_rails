ccf3479b762650d369f0d3a8db468d1f96ec4cf7
Heather Miller <heather.miller114@gmail.com>
2019-07-11 11:55:19 -0400

    adds low_to_high scope method for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,7 @@ class Review < ApplicationRecord
     validates :content, presence: true, length: { maximum: 500 }
     validates_numericality_of :star_rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5 
 
-    scope :high_to_low, -> { order(star_rating: :desc)}
+    scope :high_to_low, -> { order(star_rating: :desc) }
+    scope :low_to_high, -> { order(star_rating: :asc) }
 
 end
