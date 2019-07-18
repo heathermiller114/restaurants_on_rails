183dcf0971c848df9d484ab5e91c2776d2f37155
Heather Miller <heather.miller114@gmail.com>
2019-07-17 22:56:09 -0400

    moved show reviews logic to helper method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,3 +1,11 @@
 module ReviewsHelper
-    
+
+    def show_reviews(reviews, restaurant)
+        if @reviews == []
+            render partial: "empty", locals: { restaurant: @restaurant }
+        else
+            render partial: "reviews", locals: { reviews: @reviews }
+        end
+    end
+
 end
